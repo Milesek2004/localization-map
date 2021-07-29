1a5c8827367d2da143a7d22ea98a46df237d7e46
Milesek2004 <selimam2004@gmail.com>
2021-07-29 12:59:03 +0200

    Added MongoDB and deleted localizations.db

--- index.js
+++ index.js
@@ -1,43 +1,60 @@
 const express = require('express')
-const Datastore = require('nedb')
+const mongoClient = require('mongodb').MongoClient;
+const url = process.env.MONGODB_URI || "mongodb+srv://admin:sfis233aBOFb1D8F@localizationdb.1vnce.mongodb.net/localizationdb?retryWrites=true&w=majority";
+const dbname = "Localizationdb";
 const path = require('path');
 require('dotenv').config();
 const app = express()
+
+const ObjectId = require('mongodb').ObjectId; 
 
 app.use(express.static('public'));
 app.use(express.json({limit: '1mb'}));
 
 const port = process.env.PORT || 3000;
 
-const database = new Datastore('localizations.db');
-database.loadDatabase();
+let  db;
 
-app.post('/add',(req,res)=>{
-    console.log(req.body);
-    try{
-        const pos_time = {
-            latitude: req.body.lat,
-            longitude: req.body.lon,
-            description: req.body.description,
-            time:Date.now()
-        };
-        console.log(pos_time);
-        database.insert(pos_time);
-        res.json({status:"Location was added",values: pos_time});
-    }catch(err){
-        console.warn(err);
-    }
-});
-
-app.get('/all',(req,res)=>{
-    database.find({},(err,data)=>{
+function connect(){
+    mongoClient.connect(url,{}, (err,client)=>{
         if(err){
-            console.error(err);
+            console.log("Something went wrong with database")
+            console.log(err)
             res.end();
             return;
         }
-        res.json(data);
+        console.log("Database connection is ok");
+        db = client.db(dbname);
+    })
+}
+
+connect();
+
+app.post('/add',(req,res)=>{
+    console.log(req.body);
+    
+    const pos_time = {
+        latitude: req.body.lat,
+        longitude: req.body.lon,
+        description: req.body.description,
+        time:Date.now()
+    };
+    db.collection('localizations').insertOne(pos_time,(err,results)=>{
+        if(err) console.log(err);
+        else{
+            console.log(results);
+            res.json({status:"Location was added"});
+        }
     });
+    
+});
+
+app.get('/all',(req,res)=>{
+    db.collection('localizations').find({}).toArray((err, data) => {
+        if(err) console.log(err);
+        console.log(data);
+        res.json(data);
+    })
 });
 
 app.get('/key',(req,res)=>{
@@ -46,14 +63,12 @@ app.get('/key',(req,res)=>{
 
 app.get('/all/:id',(req,res)=>{
     console.log(`Searching for: ${req.params.id}`);
-    database.find({_id: req.params.id},(err,data)=>{
-        if(err){
-            console.error(err);
-            res.end();
-            return;
-        }
+    
+    db.collection('localizations').find({_id: ObjectId("6102798fa2cf1d9ec36395eb")}).toArray((err, data) => {
+        if(err) console.log(err);
+        console.log(data);
         res.json(data);
-    });
+    })
 });
 
 app.get('/pos/:latlon',(req,res)=>{
@@ -63,14 +78,12 @@ app.get('/pos/:latlon',(req,res)=>{
     const lat = parseFloat(position[0]);
     const lon = parseFloat(position[1]);
 
-    database.find({"latitude": lat,"longitude": lon},(err,data)=>{
-        if(err){
-            console.error(err);
-            res.end();
-            return;
-        }
+    
+    db.collection('localizations').find({"latitude": lat,"longitude": lon}).toArray((err, data) => {
+        if(err) console.log(err);
+        console.log(data);
         res.json(data);
-    });
+    })
 });
 
 app.get('/map',(req,res)=>{
